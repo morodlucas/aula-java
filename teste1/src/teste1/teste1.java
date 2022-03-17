6e9e84fe6aa944260f5e98a7b75c14af282a40a3
Logon Aluno <logonrmlocal@fiap.com.br>
2022-03-17 09:02:40 -0300

    Aula17/3
    teste calculadora eclipse em java

---
+++ teste1/src/teste1/teste1.java
@@ -0,0 +1,17 @@
+package teste1;
+
+public class teste1 {
+
+	public static void main(String[] args) {
+		int x=50, y=10, z;
+		z=x+y+y*y;
+		
+		double a=6.9, b=4.2, c;
+		c=a+b;
+		
+		System.out.println("resultado - " + z);
+		System.out.println("resultado 2 -  " + c);
+
+	}
+
+}

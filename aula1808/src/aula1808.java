d813b9c06d83039cdbf38da12ba77a5b70eb6bf7
Lucas Dias <lucas.dias.moro@gmail.com>
2022-08-18 11:37:45 -0300

    add

---
+++ aula1808/src/aula1808.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class aula1808 {
+
+	public static void main(String[] args) {
+
+		Scanner kb = new Scanner(System.in);
+		double media, maior = 0;
+		
+		double[] temperatura = new double[7];
+		String[] dia = {"Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"};
+		
+		for (int i = 0; i < dia.length; i++) {
+			System.out.print("Temperatura do(a) " + dia[i] + ": ");
+			temperatura[i] = kb.nextDouble();
+			System.out.println("-------------------------");
+		}
+		
+		media = (temperatura[1] + temperatura[2] + temperatura[3] + temperatura[4] + temperatura[5] + temperatura [6] + temperatura[0])/7;
+		
+		System.out.println("-------------------------------------");
+		
+		System.out.println("Média da semana -> " + String.format(media, %.2f));
+		
+		for (int i = 0; i < dia.length; i++) {
+			if(temperatura[i] > media) {
+				System.out.println("Acima da média -> " + dia[i]);;
+				System.out.println("-------------------------------------");
+			}
+		}
+		
+		
+		kb.close();
+		
+	}
+
+}
